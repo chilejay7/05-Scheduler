b2fe2e411ccfe1bdbe78ff4350ae66295bcc73e6
Cody Burkholder <codyburk7@yahoo.com>
2023-09-18 10:57:18 -0600

    added time div elements for working hours

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,20 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+const day = $('#currentDay');
+
+// $(day).text(today.format('MMM D, YYYY'));
+
+$(document).ready(function(){
+  $('.saveBtn').on('click', function(e) {
+    console.log('clicked', e);
+    let calendarInput = $('#hour-12 textarea').val().trim()
+    console.log(calendarInput);
+  })
+  
+})
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +35,4 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
