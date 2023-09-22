b893da6a4c9f8e9c84e3f4867d7b81c440ac9764
Cody Burkholder <codyburk7@yahoo.com>
2023-09-22 16:34:20 -0600

    added storage retrieval

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
 const currentDate = dayjs().format(`dddd D, YYYY`);
 
 const buttons = $('.saveBtn')
+
+// This was moved to the global scope in order to provide the object to muiltiple functions.
+const timeBlocks = $('.hour')
 
 $(document).ready(function(){
 
@@ -13,35 +11,17 @@ $(document).ready(function(){
   $('#currentDay').text(currentDate);
   
   // This dipslays the current time updated every second.  The seconds can be removed and the interval changed to a longer duration if seconds are not displayed.
-  currentTime = () => {
+  const currentTime = () => {
     let time = dayjs().format(`h:mm:ss a`)
     $('#currentTime').text(time)
   }
-  setInterval(currentTime, 1000)
+
+  setInterval(currentTime, 1000);
 
   setColors();
-})
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+  getData();
+})
 
 buttons.on('click', function(e) {
   // console.log('clicked', e.target);
@@ -49,17 +29,27 @@ buttons.on('click', function(e) {
   console.log(e.target.parentElement.id);
   const parentId = e.target.parentElement.id
   
-
   let calendarInput = $(`#${parentId} textarea`).val().trim();
   console.log(calendarInput);
 
   storeData(parentId, calendarInput);
 })
 
+// This function saves data in local storage and accepts two arguments.  The function is called in the event listener when a save button is clicked and the parentId and calendarInput variables are passed to it as arguments.
 const storeData = (id, input) => {
   localStorage.setItem(id, input)
 }
 
+// This getData function will retrieve the values from local storage.  It is called in the document.ready function, allowing data to persist after refresh.  All values are stored using the element's hour id as the key for easy identification.  This is the same technique used in the storeData function.
+const getData = () => {
+  for (let i = 0; i < timeBlocks.length; i++) {
+    let timeBlockId = timeBlocks[i].parentElement.id
+    let getStorage = localStorage.getItem(timeBlockId)
+    $(`#${timeBlockId} textarea`).val(getStorage)
+    // console.log(localStorage.getItem(timeBlockId))
+  }
+  
+}
 
 // This function is called within the setColors function and the return value is saved to the hourNow variable.
 setHour = () => {
@@ -80,16 +70,17 @@ setHour = () => {
 }
 
 // This function is called within the document.ready function.
-setColors = () => {
-  const timeBlocks = $('.hour')
+const setColors = () => {
+  // This variable was moved to the global scope as other functions required it.
+  // const timeBlocks = $('.hour')
   const amPm = dayjs().format(`a`)
 
   let hourNow = setHour();
   
   for (let i = 0; i < timeBlocks.length; i++) {
-    console.dir(timeBlocks[i]);
+    // console.dir(timeBlocks[i]);
     let timeInt = parseInt(timeBlocks[i].innerHTML);
-    console.log(timeInt)
+    // console.log(timeInt)
     
     if (timeInt > hourNow && amPm === 'am') {
 
@@ -126,24 +117,3 @@ setColors = () => {
   }
 }
 }
-
-
-// const morning = $('.morning')
-// const afternoon = $('.afternoon')
-
-// setMorning = () => {
-//   for (let i = 0; i < morning.length; i++) {
-//   morning[i].nextSibling.nextElementSibling.className += ' past'
-// }
-// }
-
-// setAfternoon = () => {
-//   for (let i = 0; i < morning.length; i++) {
-//     if (timeInt > hourNow) {
-//   afternoon[i].nextSibling.nextElementSibling.className += ' future'
-//   } else if (timeInt === hourNow) {
-//     afternoon[i].nextSibling.nextElementSibling.className += ' present'
-//   }
-// }
-// }
-
